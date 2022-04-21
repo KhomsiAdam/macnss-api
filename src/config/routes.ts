939e53c81c98832aa4b8fef65545350e3c591478
KhomsiAdam <khomsiadam@gmail.com>
2022-04-21 15:10:11 +0000

    Added client, file and medicine endpoints to the router

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,11 +3,17 @@ import express from 'express';
 import authEndpoints from '@entities/auth/endpoints';
 import adminEndpoints from '@entities/admin/endpoints';
 import userEndpoints from '@entities/user/endpoints';
+import clientEndpoints from '@entities/client/endpoints';
+import fileEndpoints from '@entities/file/endpoints';
+import medicineEndpoints from '@entities/medicine/endpoints';
 
 export const router = express.Router();
 
 router.use('/', authEndpoints);
 router.use('/admins', adminEndpoints);
 router.use('/users', userEndpoints);
+router.use('/clients', clientEndpoints);
+router.use('/files', fileEndpoints);
+router.use('/medicines', medicineEndpoints);
 
 export default router;
